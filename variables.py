98ec36d7b16442397cf7baaf35747832300839cb
Roland Archie <rolandarchie93@gmail.com>
2023-04-16 15:50:23 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+firstname = "Jane"
+surname = "Doe"
+
+print(f"My first name is {firstname}. My family name is {surname}")
+
+my_int = 50
+sentence = "The total comes to: "
+
+print(sentence + str(my_int))
+
+
+
